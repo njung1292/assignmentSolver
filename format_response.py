0a830531130413f9eee5047f1645249c5b935753
Noelle Jung <njung1292@gmail.com>
2014-11-23 18:19:19 -0500

    indents to spaces

--- format_response.py
+++ format_response.py
@@ -3,8 +3,8 @@ import xlrd, xlwt, re
 ## Converts the real data into a format more like Survey Monkey data
 
 SEMINAR_LIST = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",\
-				"F11", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9",\
-				"S10"]
+                "F11", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9",\
+                "S10"]
 num_seminars = len(SEMINAR_LIST)
 
 responseBk      = xlrd.open_workbook("test data/FS_response_data.xlsx")
@@ -24,7 +24,7 @@ surveyMonkeySht.write(0,7,"Last Name")
 surveyMonkeySht.write(0,8,"Custom Data")
 surveyMonkeySht.write(0,9,"Ranking results")
 for i in xrange(num_seminars):
-	surveyMonkeySht.write(1,9+i,SEMINAR_LIST[i])
+    surveyMonkeySht.write(1,9+i,SEMINAR_LIST[i])
 
 # Get raw ranking values
 num_rows = responseSht.nrows - 1
@@ -36,13 +36,13 @@ vals = []
 # Put raw ranking values into arrays
 p = re.compile("(\d*)[^\d]*")
 for resp in responses:
-	matches = [int(c) for c in p.findall(resp) if c!='']
-	vals.append(matches)
+    matches = [int(c) for c in p.findall(resp) if c!='']
+    vals.append(matches)
 
 # Write ranking values into spreadsheet
 for i in range(len(vals)):
-	surveyMonkeySht.write(2+i,0,i+1)
-	for j in range(len(vals[i])):
-		surveyMonkeySht.write(2+i,8+vals[i][j],j+1)
+    surveyMonkeySht.write(2+i,0,i+1)
+    for j in range(len(vals[i])):
+        surveyMonkeySht.write(2+i,8+vals[i][j],j+1)
 
 surveyMonkeyBk.save("test data/FS_response_data_formatted.xls")
