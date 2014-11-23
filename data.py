a6c591469071f3bbca9e1755039bad573f8e7d18
Noelle Jung <njung1292@gmail.com>
2014-11-23 17:15:15 -0500

    Changed the data class to be more like data_b and addded a factory method (also got rid of dataGen.py)

--- data.py
+++ data.py
@@ -1,62 +1,102 @@
 from collections import Counter
 import logging
 import operator
+import random
+
+MIN_ORD = 97
 
 class Data:
     """ Class that contains data (e.g. names, number of students, seminar lists)"""
 
-    def __init__(self, names, top_fives, fall_list, spring_list):
+    def __init__(self, rank_matrix, year_list):
         """Create a new instance"""
-
         # Set data fields
-        self.names            = names              # Names of students (strings)
-        self.top_fives        = top_fives          # Top 5 rankings (int indices)
-        self.fall_list        = fall_list          # Fall seminar names
-        self.spring_list      = spring_list        # Spring seminar names
-        self.year_list        = fall_list + spring_list
-        self.num_students     = len(top_fives)
-        self.num_seminars     = len(fall_list) + len(spring_list)
+        self.rank_matrix      = []
+        self.year_list        = year_list
+        self.names            = range(1, len(rank_matrix)+1)
+        self.num_students     = len(rank_matrix)
+        self.num_seminars     = len(year_list)
         self.popular_seminars = [] # Most popular seminars (int index, int count)
 
         # Rank the seminars by popularity
         seminar_ctr = Counter()
         for i in range(self.num_students):
-            for j in range(len(self.top_fives[i])):
-                seminar_ctr[self.top_fives[i][j]] += 1
+            for j in range(self.num_seminars):
+                if (rank_matrix[i][j] < 100):
+                    seminar_ctr[j] += 1
         self.popular_seminars = seminar_ctr.most_common()
         logging.debug("Popular seminars: " + str(self.popular_seminars))
 
-    def make_matrix(self):
-        """Convert top five rankings (n x m) into an (n x n) matrix."""
-        logging.info("Converting rankings to matrix form...")
-
-        rankMatrix = []
-        seminarSize = self.num_students / self.num_seminars
-        numExtra = self.num_students % self.num_seminars
-        for i in range(self.num_students):
-            row = [100]*self.num_seminars
-            topSeminars = self.top_fives[i]
-            for j in range(5):
-                row[topSeminars[j]] = j+1
-            row *= seminarSize
-            for popularSeminar in self.popular_seminars[0:numExtra]:
-                row.append(self.__getRank(popularSeminar[0], topSeminars))
-            rankMatrix.append(row)
-
-        return rankMatrix
+        # Extend the rank matrix
+        seminar_size = self.num_students / self.num_seminars
+        num_extra = self.num_students % self.num_seminars
+        for row in rank_matrix:
+            row *= seminar_size
+            for popular_seminar in self.popular_seminars[0:num_extra]:
+                row.append(row[popular_seminar[0]])
+            self.rank_matrix.append(row)
 
     def col_to_sem(self, col):
         """Converts a rank matrix column to the seminar name."""
-        seminarSize = self.num_students / self.num_seminars
+        seminar_size = self.num_students / self.num_seminars
         c = col % self.num_seminars
-        if ((col / self.num_seminars) >= seminarSize):
+        if ((col / self.num_seminars) >= seminar_size):
             return self.year_list[self.popular_seminars[c][0]]
         else:
             return self.year_list[c]
 
-    def __getRank(self, seminar, topSeminars):
-        """Returns a students ranking of a given seminar"""
-        try:
-            return topSeminars.index(seminar) + 1
-        except ValueError:
-            return 100
+    @staticmethod
+    def factory(num_students, num_seminars):
+        """ Returns a randomly generated instance of Data_b given the number of
+            students and seminars """
+        fall_list = []
+        spring_list = []
+
+        # Generate a list of fall and spring classes
+        for i in range(num_seminars/2):
+            fall_list.append("F" + '%02d'%(i+1))
+            spring_list.append("S" + '%02d'%(i+1))
+        if (num_seminars % 2 == 1):
+            fall_list.append("F" + str(num_seminars/2 + 1))    
+
+        # Generate a list of names
+        assert num_students < 26*26
+        names = ['']*num_students
+        for i in range(num_students):
+            name = str(chr(MIN_ORD + i/26)) + str(chr(MIN_ORD + i))
+            names[i] = name
+
+        # Generate a matrix of seminar rankings for each student
+        rank_matrix = [[]]*num_students
+        num_fall   = len(fall_list)
+        num_spring = len(spring_list)
+        seminar_size = num_students / num_seminars
+        num_extra = num_students % num_seminars
+        for i in range(num_students):
+            row = [100]*num_seminars
+            top_five = [-1]*5
+
+            # fall semester
+            assert num_fall > 3
+            f1, f2, r1 = random.sample(range(num_fall), 3)
+            top_five[0] = f1
+            top_five[1] = f2
+
+            # spring semester
+            assert num_spring > 2
+            s1, s2 = random.sample(range(num_fall, num_fall + num_spring), 2)
+            top_five[2] = s1
+            top_five[3] = s2
+
+            # last seminar
+            last_sem = r1 if (r1%2 == 0) else (r1 + num_fall - 1)
+            assert last_sem < num_seminars
+            top_five[4] = last_sem
+            
+            random.shuffle(top_five)
+
+            for j in range(5): # We are guaranteed to need to check 5
+                row[top_five[j]] = j+1
+            rank_matrix[i] = row
+
+        return Data(rank_matrix, fall_list + spring_list)
