a6c513d2f2950a02fa037457c84d4e54e76a3668
Noelle Jung <njung1292@gmail.com>
2014-11-22 23:21:01 -0500

    Renamed variables and made some functions more concise

--- data.py
+++ data.py
@@ -5,64 +5,54 @@ import operator
 class Data:
     """ Class that contains data (e.g. names, number of students, seminar lists)"""
 
-    def __init__(self, names, topFives, fallList, springList):
+    def __init__(self, names, top_fives, fall_list, spring_list):
         """Create a new instance"""
 
         # Set data fields
-        self.names           = names
-        self.topFives        = topFives
-        self.fallList        = fallList
-        self.springList      = springList
-        self.yearList        = fallList + springList
-        self.numStudents     = len(topFives)
-        self.numSeminars     = len(fallList) + len(springList)
-        self.popularSeminars = []
+        self.names            = names              # Names of students (strings)
+        self.top_fives        = top_fives          # Top 5 rankings (int indices)
+        self.fall_list        = fall_list          # Fall seminar names
+        self.spring_list      = spring_list        # Spring seminar names
+        self.year_list        = fall_list + spring_list
+        self.num_students     = len(top_fives)
+        self.num_seminars     = len(fall_list) + len(spring_list)
+        self.popular_seminars = [] # Most popular seminars (int index, int count)
 
         # Rank the seminars by popularity
-        seminarDict = Counter()
-        for i in range(self.numStudents):
-            for j in range(len(self.topFives[i])):
-                seminarDict[self.topFives[i][j]] += 1
-        self.popularSeminars = seminarDict.most_common()
-        logging.debug("Popular seminars: " + str(self.popularSeminars))
+        seminar_ctr = Counter()
+        for i in range(self.num_students):
+            for j in range(len(self.top_fives[i])):
+                seminar_ctr[self.top_fives[i][j]] += 1
+        self.popular_seminars = seminar_ctr.most_common()
+        logging.debug("Popular seminars: " + str(self.popular_seminars))
 
-    def makeMatrix(self):
+    def make_matrix(self):
         """Convert top five rankings (n x m) into an (n x n) matrix."""
-
-        logging.info("\n  Converting rankings to matrix form...")
+        logging.info("Converting rankings to matrix form...")
 
         rankMatrix = []
-        seminarSize = self.numStudents / self.numSeminars
-        for i in range(self.numStudents):
-            row   = []
-            count = 0
-            topSeminars = self.topFives[i]
-            for seminar in self.yearList:
-                if (count < 5):
-                    rank = self.__getRank(seminar, topSeminars)
-                    row.append(rank)
-                    if (rank != 100):
-                        count += 1
-                else:
-                    numRemaining = self.numSeminars - i
-                    row += [100]*numRemaining
+        seminarSize = self.num_students / self.num_seminars
+        numExtra = self.num_students % self.num_seminars
+        for i in range(self.num_students):
+            row = [100]*self.num_seminars
+            topSeminars = self.top_fives[i]
+            for j in range(5):
+                row[topSeminars[j]] = j+1
             row *= seminarSize
-            numExtra = self.numStudents % self.numSeminars
-            for popularSeminar in self.popularSeminars[0:numExtra]:
+            for popularSeminar in self.popular_seminars[0:numExtra]:
                 row.append(self.__getRank(popularSeminar[0], topSeminars))
             rankMatrix.append(row)
 
         return rankMatrix
 
-    def columnToSeminar(self, col):
+    def col_to_sem(self, col):
         """Converts a rank matrix column to the seminar name."""
-
-        seminarSize = self.numStudents / self.numSeminars
-        c = col % self.numSeminars
-        if ((col / self.numSeminars) >= seminarSize):
-            return self.popularSeminars[c][0]
+        seminarSize = self.num_students / self.num_seminars
+        c = col % self.num_seminars
+        if ((col / self.num_seminars) >= seminarSize):
+            return self.year_list[self.popular_seminars[c][0]]
         else:
-            return self.yearList[c]
+            return self.year_list[c]
 
     def __getRank(self, seminar, topSeminars):
         """Returns a students ranking of a given seminar"""
